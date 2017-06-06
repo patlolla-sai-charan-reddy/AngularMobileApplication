98033e4c67bc3137f75395af3f9a6016498cf759
Sai Charan Patlolla <scrpatlolla@gmail.com>
2017-06-05 21:20:38 -0400

    E2E Tests

--- app/greet-user/greet-user.component.js
+++ app/greet-user/greet-user.component.js
@@ -3,6 +3,6 @@ angular.
 	component('greetUser', {
     templateUrl: 'greet-user/greet-user.template.html',
     controller: function GreetUserController() {
-      this.user = 'Welcome to Application';
+      this.user = 'Welcome to Mobile Application';
     }
   });
--- app/greet-user/greet-user.spec.js
+++ app/greet-user/greet-user.spec.js
@@ -1,12 +1,12 @@
 describe('greetUser', function() {
-	beforeEach(module("phonecatApp"));
+	beforeEach(module("greetUser"));
 
 	describe('GreetUserController', function() {
 		it('should create a `phones` model with 3 phones',
 			inject(function($componentController) {
 				var ctrl = $componentController('greetUser');
 
-				expect(ctrl.user).toBe("User");
+				expect(ctrl.user).toBe("Welcome to Mobile Application");
 			}));
 	})
 })
