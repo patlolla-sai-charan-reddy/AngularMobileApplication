a2d3c8ccc8bacf67bf7dca874a65b47cd400d90a
Sai Charan Patlolla <scrpatlolla@gmail.com>
2017-06-06 10:44:14 -0400

    JSOn Config

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -2,13 +2,14 @@ angular.
 	module('phoneList').
 	component('phoneList', {
 		templateUrl: 'phone-list/phone-list.template.html',
-		controller: function PhoneListController($http) {
-			var self = this;
-			self.orderProp = 'age';
+		controller: ['$http',
+			function PhoneListController($http) {
+				var self = this;
+				self.orderProp = 'age';
 
-			$http.get('phones/phones.json').then(function(response) {
-				console.log("response", response);
-				self.phones = response.data;
-			});
-		}
+				$http.get('phones/phones.json').then(function(response) {
+					self.phones = response.data;
+				});
+			}
+		]
 	});
