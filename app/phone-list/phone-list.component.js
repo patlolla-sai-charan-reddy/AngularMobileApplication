0d6f65862ed45e01796ed6f0dbfaaae6321a86dd
Sai Charan Patlolla <scrpatlolla@gmail.com>
2017-06-06 09:33:56 -0400

    http service

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -2,23 +2,13 @@ angular.
 	module('phoneList').
 	component('phoneList', {
 		templateUrl: 'phone-list/phone-list.template.html',
-		controller: function PhoneListController() {
-			this.phones = [
-				{
-					name: 'Nexus S',
-					details: 'Fast just got faster with Nexus S.',
-					age: 1
-				}, {
-					name: 'Motorola XOOM™ with Wi-Fi',
-					details: 'The Next, Next Generation tablet.',
-					age: 2
-				}, {
-					name: 'MOTOROLA XOOM™',
-					details: 'The Next, Next Generation tablet.',
-					age: 3
-				}
-			];
+		controller: function PhoneListController($http) {
+			var self = this;
+			self.orderProp = 'age';
 
-			this.orderProp = 'age';
+			$http.get('phones/phones.json').then(function(response) {
+				console.log("response", response);
+				self.phones = response.data;
+			});
 		}
 	});
