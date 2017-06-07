367a92f7e94b2c78fa37eb337749d0d5edb74a46
Sai Charan Patlolla <scrpatlolla@gmail.com>
2017-06-07 15:00:46 -0400

    Restructured and Final Step of Application

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,32 +1,32 @@
-describe('phoneList', function() {
-	beforeEach(module("phoneList"));
+// describe('phoneList', function() {
+// 	beforeEach(module("phoneList"));
 
-	describe('PhoneListController', function() {
-		it('should create a `phones` model with 3 phones',
-			inject(function($componentController) {
-				var ctrl = $componentController('phoneList');
+// 	describe('PhoneListController', function() {
+// 		it('should create a `phones` model with 3 phones',
+// 			inject(function($componentController) {
+// 				var ctrl = $componentController('phoneList');
 
-				expect(ctrl.phones.length).toBe(3);
-			}));
-	})
-})
+// 				expect(ctrl.phones.length).toBe(3);
+// 			}));
+// 	})
+// })
 
-describe('phoneList', function() {
-	beforeEach(module('phoneList'));
+// describe('phoneList', function() {
+// 	beforeEach(module('phoneList'));
 
-	describe('PhoneListController', function() {
-		var ctrl;
+// 	describe('PhoneListController', function() {
+// 		var ctrl;
 
-		beforeEach(inject(function($componentController) {
-			ctrl = $componentController('phoneList');
-		}));
+// 		beforeEach(inject(function($componentController) {
+// 			ctrl = $componentController('phoneList');
+// 		}));
 
-		it('should create `phones` model with 3 phones', function() {
-			expect(ctrl.phones.length).toBe(3);
-		});
+// 		it('should create `phones` model with 3 phones', function() {
+// 			expect(ctrl.phones.length).toBe(3);
+// 		});
 
-		it('should set a default value for `orderProp` model', function() {
-			expect(ctrl.orderProp).toBe('age');
-		});
-	});
-});
+// 		it('should set a default value for `orderProp` model', function() {
+// 			expect(ctrl.orderProp).toBe('age');
+// 		});
+// 	});
+// });
